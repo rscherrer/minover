e4b705e9758a1c64aa91c75a22ad6086b5c90e95
rscherrer <raphael.scherrer@evobio.eu>
2019-01-14 09:58:02 +0100

    Add files via upload

--- main.m
+++ main.m
@@ -6,17 +6,24 @@
 clear, clc, close all
 
 % Parameters
-ndims = 10;
-datastd = 5; % standard deviation for generating points
+nvalues = 100;
+[~, nns] = size(nvalues);
+datastd = 1; % standard deviation for generating points
 nreplicates = 10;
 tmax = 1000; % max training time
 alphavalues = 0.25:0.25:3;
 [~, nalphas] = size(alphavalues);
 
 % Initialize vector of generalization errors
-generrors = zeros(1, nalphas);
+generrors = zeros(nns, nalphas);
+controls = zeros(nns, nalphas);
 
-% Loop through values of alpha
+% Loop through numbers of dimensions
+for k = 1:nns
+    
+    ndims = nvalues(k);
+    
+    % Loop through values of alpha
 for j = 1:nalphas
     
     % Current alpha value
@@ -30,6 +37,7 @@ for j = 1:nalphas
     
     % Initialize the error for the current set of replicates
     generror = 0;
+    controlerror = 0;
     
     % Loop through replicates
     for i = 1:nreplicates
@@ -50,16 +58,33 @@ for j = 1:nalphas
         % Train the perceptron with the minover algorithm and record the
         % generalization error
         generror = generror + train_minover(dataset, labels, teacher, tmax);
+        
+        % Control wrt error of Rosenblatt's perceptron
+        controlerror = controlerror + train_rosenblatt(dataset, labels, teacher, tmax);
 
     end
 
     % Compute the average error across replicates
-    generrors(j) = generror / nreplicates;
+    generrors(k,j) = generror / nreplicates;
+    controls(k,j) = controlerror / nreplicates;
+    
+end
     
 end
 
-% Plot
-plot(alphavalues, generrors)
-title('Minover performance')
-xlabel('\alpha (= P/N)')
-ylabel('Generalization error')
+for i = 1:nns
+    
+    plot(alphavalues, generrors(i,:))
+    if i == 1
+        xlim([0 max(alphavalues)])
+        ylim([0 1])
+        title('Minover performance')
+        xlabel('\alpha (= P/N)')
+        ylabel('Generalization error')
+        hold on
+    end
+end
+%legend('N = 50','N = 100', 'N = 200')
+hold off
+saveas(gcf, "error_vs_alpha", "pdf")
+
